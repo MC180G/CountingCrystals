4aa56bc362f07c0bf79ae027581638bdebb8c43f
Devvo Matto <devvomatto@gmail.com>
2020-09-21 12:40:39 -0400

    finished JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,3 @@
-$(document).ready(function() {
-
     let crystal = {
         red: {
             name:"red",
@@ -37,18 +35,20 @@ const startGame = function() {
 
 
  // * The random number shown at the start of the game should be between 19 - 120.
-
+ 
  targetScore = randomNumber(19, 120);
  
-$("#rdm-num-prm").text(targetScore);
-
-// * Each crystal should have a random hidden value between 1 - 12. -->
-crystal.red.value = randomNumber(1, 12); 
-crystal.blue.value = randomNumber(1, 12);  
-crystal.green.value = randomNumber(1, 12);  
-crystal.yellow.value = randomNumber(1, 12); 
-
-$("#ply-scr").text(currentScore);
+ $("#rdm-num-prm").text(targetScore);
+ 
+ // * Each crystal should have a random hidden value between 1 - 12. -->
+ crystal.red.value = randomNumber(1, 12); 
+ crystal.blue.value = randomNumber(1, 12);  
+ crystal.green.value = randomNumber(1, 12);  
+ crystal.yellow.value = randomNumber(1, 12); 
+ //   * Your game will hide this amount until the player clicks a crystal.
+ 
+ //   * When they do click one, update the player's score counter. -->
+ $("#ply-scr").text(currentScore);
 
 console.log("Target Score = " + targetScore);
 console.log("Red = " + crystal.red.value);
@@ -59,30 +59,56 @@ console.log("Yellow = " + crystal.yellow.value);
 };
 
 
-startGame();
-// <!-- * When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
-$("#btn1").on("click", function() {
- var crystalValue = ($(this).attr("data-crystalvalue"));
-crystalValue = parseInt(crystalValue);
- counter += crystalValue;
- $("#ply-scr").text(counter);       
- 
- console.log (counter);
+const checkWin = function() {
 
+if (currentScore > targetScore) {
+    alert("Sorry, loser!");
+    losses++;
+    $("#lossCount").text(losses);
+    startGame();
+}
+else if (currentScore === targetScore) {
+    alert("You're a Big Winner!");
+    wins++;
+    $("#winCount").text(wins);
+    startGame();
+}
+};
+
+
+
+// <!-- * When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
+
+const addValues = function(clickedCrystal) {
+
+    currentScore += clickedCrystal.value;
+
+    $("#ply-scr").text(currentScore);
+
+    checkWin();
+}
+
+startGame();
+
+$("#red").click(function() {
+ addValues(crystal.red);  
+});
+
+$("#blue").click(function() {
+ addValues(crystal.blue);  
+});
+
+$("#green").click(function() {
+ addValues(crystal.green);  
+});
+
+$("#yellow").click(function() {
+ addValues(crystal.yellow);  
 });
 
 
-//   * Your game will hide this amount until the player clicks a crystal.
-//   * When they do click one, update the player's score counter. -->
 
-// <!-- * The player wins if their total score matches the random number from the beginning of the game.
-// if randomCrystal === randomPrime
-// $().text("You're a big winner!")
-
-// // // <!--* The player loses if their score goes above the random number.
-// if randomCrystal > randomPrime
-// $().text("You blew it, stupid!")// * There will be four crystals displayed as buttons on the page.
-
+// * There will be four crystals displayed as buttons on the page.
 
 // * The player wins if their total score matches the random number from the beginning of the game.
 
@@ -93,4 +119,3 @@ crystalValue = parseInt(crystalValue);
 //   * When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
 
 // * The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
-});
